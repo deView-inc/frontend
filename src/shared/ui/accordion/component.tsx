dd71e0e99feb2bc0569894134cdb0ccef0b201e2
soorq <fantastfad830@gmail.com>
2026-08-16 22:03:37 +0300

    feat: newest ui components, refactor oldest same styles

--- src/shared/ui/accordion/component.tsx
+++ src/shared/ui/accordion/component.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Accordion as AccordionPrimitive } from '@base-ui/react/accordion';
-import { CaretDownIcon, CaretUpIcon } from '@phosphor-icons/react';
+import { CaretDownIcon, CaretUpIcon, PlusIcon } from '@phosphor-icons/react';
 import { cn } from '~&/shared/lib/utils';
 
 function Accordion({ className, ...props }: AccordionPrimitive.Root.Props) {
     return (
         <AccordionPrimitive.Root
             data-slot="accordion"
-            className={cn('flex w-full flex-col', className)}
+            className={cn('flex w-full gap-3 flex-col', className)}
             {...props}
         />
     );
@@ -18,7 +18,7 @@ function AccordionItem({ className, ...props }: AccordionPrimitive.Item.Props) {
     return (
         <AccordionPrimitive.Item
             data-slot="accordion-item"
-            className={cn('not-last:border-b', className)}
+            className={cn('border border-border bg-background rounded px-5', className)}
             {...props}
         />
     );
@@ -26,23 +26,19 @@ function AccordionItem({ className, ...props }: AccordionPrimitive.Item.Props) {
 
 function AccordionTrigger({ className, children, ...props }: AccordionPrimitive.Trigger.Props) {
     return (
-        <AccordionPrimitive.Header className="flex">
+        <AccordionPrimitive.Header className="flex items-center">
             <AccordionPrimitive.Trigger
                 data-slot="accordion-trigger"
                 className={cn(
-                    'group/accordion-trigger relative flex flex-1 items-start justify-between rounded-none border border-transparent py-2.5 text-left text-xs font-medium transition-all outline-none hover:underline focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring/50 focus-visible:after:border-ring aria-disabled:pointer-events-none aria-disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4 **:data-[slot=accordion-trigger-icon]:text-muted-foreground',
+                    'group/accordion-trigger relative flex flex-1 items-start justify-between rounded py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring/50 focus-visible:after:border-ring aria-disabled:pointer-events-none aria-disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4 **:data-[slot=accordion-trigger-icon]:text-muted-foreground',
                     className,
                 )}
                 {...props}
             >
                 {children}
-                <CaretDownIcon
+                <PlusIcon
                     data-slot="accordion-trigger-icon"
-                    className="pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden"
-                />
-                <CaretUpIcon
-                    data-slot="accordion-trigger-icon"
-                    className="pointer-events-none hidden shrink-0 group-aria-expanded/accordion-trigger:inline"
+                    className="pointer-events-none shrink-0 transform transition-transform group-aria-expanded/accordion-trigger:rotate-45"
                 />
             </AccordionPrimitive.Trigger>
         </AccordionPrimitive.Header>
