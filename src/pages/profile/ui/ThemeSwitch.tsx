07329f805dc69d6ba9fd5290f035d0f57ea35beb
l1ha4 <l1hach.main@gmail.com>
2026-09-09 02:46:43 +0300

    refactor(profile): replace tabs with segmented

--- src/pages/profile/ui/ThemeSwitch.tsx
+++ src/pages/profile/ui/ThemeSwitch.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useCallback } from 'react';
 import { type Theme, useTheme } from '~&/shared/lib/theme';
-import { Icon, type PhosphorIconName, Tabs, TabsList, TabsTrigger } from '~&/shared/ui';
+import { Icon, type PhosphorIconName, Segmented, SegmentedItem } from '~&/shared/ui';
 
 const THEME_OPTIONS: { value: Theme; label: string; icon: PhosphorIconName }[] = [
     { icon: 'Sun', label: 'Светлая', value: 'light' },
@@ -13,27 +12,24 @@ const THEME_OPTIONS: { value: Theme; label: string; icon: PhosphorIconName }[] =
 export function ThemeSwitch() {
     const { setTheme, theme } = useTheme();
 
-    const onValueChangeFn = useCallback((value: string) => setTheme(value as Theme), [setTheme]);
-
     return (
-        <Tabs
+        <Segmented
+            variant="buttons"
             value={theme}
-            onValueChange={onValueChangeFn}
+            onValueChange={setTheme}
         >
-            <TabsList variant="buttons">
-                {THEME_OPTIONS.map(({ icon, label, value }) => (
-                    <TabsTrigger
-                        key={value}
-                        value={value}
-                    >
-                        <Icon
-                            name={icon}
-                            size={18}
-                        />
-                        {label}
-                    </TabsTrigger>
-                ))}
-            </TabsList>
-        </Tabs>
+            {THEME_OPTIONS.map(({ icon, label, value }) => (
+                <SegmentedItem
+                    key={value}
+                    value={value}
+                >
+                    <Icon
+                        name={icon}
+                        size={18}
+                    />
+                    {label}
+                </SegmentedItem>
+            ))}
+        </Segmented>
     );
 }
